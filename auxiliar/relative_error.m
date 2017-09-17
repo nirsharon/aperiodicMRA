fafd24c0c276ba800e4d8ab2ff690bbac7d848d6
nirsharon <nsharon@math.princeton.edu>
2017-09-17 17:10:36 -0400

    Delete relative_error.m

--- auxiliar/relative_error.m
+++
@@ -1,7 +0,0 @@
-function relerr = relative_error(xref, xest)
-% Relative error between xref (reference) and xest, up to circular shifts.
-
-    xest = align_to_reference(xest, xref);
-    relerr = norm(xref-xest) / norm(xref);
-
-end
