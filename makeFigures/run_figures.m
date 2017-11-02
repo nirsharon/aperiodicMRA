8b96761e066127139151f3f2c7cf942774c39d27
nirsharon <nsharon@math.princeton.edu>
2017-11-02 11:14:41 -0400

    Update run_figures.m

--- makeFigures/run_figures.m
+++ makeFigures/run_figures.m
@@ -6,12 +6,16 @@
 %
 % NS, September -- November  2017
 
-% the counter example of uniquness with periodic distribution, i.e., Proposition III.6, including figures
+% Figure I.1
+visual_fig_data
+% the counter example of uniquness with periodic distribution, i.e., Proposition III.6, including Figure III.1
 make_figure_counter_example
-% the comparison between : spectral, LS, and adapted-EM
-vary_sigma_comparison
-% in the following, make sure the number of trials is large enough. Add parallel pool if needed
+% the basis figures and calculations for Figure VII.1
+visual_demo_LS
+% in the following, make sure the number of trials is large enough. Add parallel pool if needed (Figure VII.2)
 run_EM_comparison
+% the comparison between : spectral, LS, and adapted-EM (Figure VII.3)
+vary_sigma_comparison
 
 
 % older comparisons: main_comparison, plot_EMvsLS
